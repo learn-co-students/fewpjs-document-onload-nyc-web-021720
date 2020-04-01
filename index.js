3b20e99bd724c2a65cf7546c342dfd10f9e9c77a
osgood gunawan <ogunawan@ucsd.edu>
2020-03-31 20:30:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded",function()
+{
+    let text=document.getElementById('text')
+    text.textContent="This is really cool!"
+    // text.innerHTML="This is really cool!"
+});
+
+
